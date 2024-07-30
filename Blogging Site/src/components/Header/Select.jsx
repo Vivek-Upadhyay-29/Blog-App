5dfc7783487f3c1042c4f8209e541f7ba3656d4d
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-07-30 21:14:25 +0530

    commit

---
+++ Blogging Site/src/components/Header/Select.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Select({
+    option,
+    label,
+    className,
+    ...props
+}) {
+
+  
+  return (
+    <div>Select</div>
+  )
+}
+
+export default Select
