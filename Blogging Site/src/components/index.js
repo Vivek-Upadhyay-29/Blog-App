e7d721ff403ff638d7c2d7aee53def23bd2c9900
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-10-02 01:06:20 +0530

    commit

--- Blogging Site/src/components/index.js
+++ Blogging Site/src/components/index.js
@@ -1,6 +1,12 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Logo from "./Logo.jsx";
+import Signup from "../pages/Signup.jsx";
+import Container from "./containers/Container.jsx";
 export {
+    Container,
     Header,
+    Signup,
+    Logo,
     Footer}
  
